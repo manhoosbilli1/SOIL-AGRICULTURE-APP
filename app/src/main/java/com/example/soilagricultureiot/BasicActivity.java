eff24c5e57f14bb9a08139934d73e92918863f14
baseer451 <shoaib.mustafa7@hotmail.com>
2020-07-29 02:03:25 +0500

    Added soils health and suggestion

--- app/src/main/java/com/example/soilagricultureiot/BasicActivity.java
+++ app/src/main/java/com/example/soilagricultureiot/BasicActivity.java
@@ -39,6 +39,10 @@ public class BasicActivity extends AppCompatActivity {
     TextView ph_value;
     TextView humidity_value;
     TextView smoke_value;
+    TextView phSuggestion;
+    TextView smokeSuggestion;
+    TextView moistureSuggestion;
+    TextView humiditySuggestion;
     ImageView moisture_pic, ph_pic,humidity_pic,smoke_pic;
     FirebaseAuth mAuth;
     FirebaseUser user;
@@ -52,12 +56,15 @@ public class BasicActivity extends AppCompatActivity {
         TextView moisture_text = (TextView) findViewById(R.id.moisture_text);
         TextView humidity_text = (TextView) findViewById(R.id.humidity_text);
         TextView ph_text = (TextView) findViewById(R.id.ph_text);
+        final TextView phSuggestion = (TextView) findViewById(R.id.phSuggestion);
+        final TextView smokeSuggestion = (TextView) findViewById(R.id.smokeSuggestion);
+        final TextView humiditySuggestion = (TextView) findViewById(R.id.humiditySuggestion);
+        final TextView moistureSuggestion = (TextView) findViewById(R.id.moistureSuggestion);
         TextView smoke_text = (TextView) findViewById(R.id.smoke_text);
         final TextView moisture_value = (TextView) findViewById(R.id.moisture_value);
         final TextView ph_value = (TextView) findViewById(R.id.ph_value);
         final TextView humidity_value = (TextView) findViewById(R.id.humidity_value);
         final TextView smoke_value = (TextView) findViewById(R.id.smoke_value);
-
         ImageView moisture_pic = (ImageView) findViewById(R.id.moisture_pic);
         ImageView ph_pic = (ImageView) findViewById(R.id.ph_pic);
         ImageView humidity_pic = (ImageView) findViewById(R.id.humidity_pic);
@@ -114,6 +121,60 @@ public class BasicActivity extends AppCompatActivity {
                 Float ph = phN.floatValue();
                 Float moisture= moistureN.floatValue();
                 Float humidity= humidityN.floatValue();
+
+                //setting suggestion
+
+                //trigger values
+
+                //ph more than 7 basic and less than 7 acidic, equal to 7 just water
+                //gas more than 250 gas high, less 250 normal
+                //moisture more than 900 fully water less than 100 zero water 300 to 600 optimum
+                //humidity more than 60 too humid less than 30 is less than optimum..between 60 and 30 is optimum
+
+                if(gas >= 220){   //high toxicity
+                    smokeSuggestion.setText("Toxicity is high, introduce some detox to the soil");
+                }
+                if(gas <= 220){  //normal
+                    smokeSuggestion.setText("Value Optimum, No action needed");
+
+                }
+                if(ph >= 7.2){ //basic
+                    phSuggestion.setText("PH is basic, Introduce some acidic material to soil to reduce it.");
+                }
+                if(ph <= 6.7){     //acidic
+                    phSuggestion.setText("PH is ACIDIC, introduce some basic material to the soil to reduce it.");
+
+                }
+                if(ph >= 6.7 || ph <= 7.2){   //normal
+                    phSuggestion.setText("PH is optimum, No action required");
+                }
+                if(moisture >= 800 || moisture <=1050){   //moisture high
+                    moistureSuggestion.setText("Moisture too high, Add more soil or drain water to reduce the moisture");
+
+                }
+                if(moisture >= 350 || moisture <=600) {   //optimum
+                    moistureSuggestion.setText("Moisture Optimum, No action needed");
+
+                }
+
+                if(moisture <= 300 || moisture >= 0){    //moisture low
+                    moistureSuggestion.setText("Moisture is too low, Add water to increase the moisture");
+
+                }
+                if(humidity >= 0 || humidity <= 30){ //low humidity
+                    humiditySuggestion.setText("Humidity is low, Add water to the plant more frequently");
+
+                }
+                if(humidity >30 || humidity >= 50) {  //optimum
+                    humiditySuggestion.setText("Humidity is optimum, No Action need");
+                }
+                if(humidity >= 50 || humidity <=80){  //humidity high
+                    humiditySuggestion.setText("Humidity too high, Add water infrequently");
+
+                }
+
+
+
 
 //                Float gas = dataMap.get("gas");
 //                Float ph = dataMap.get("ph");
